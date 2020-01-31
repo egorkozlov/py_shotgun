0af091a7641808e93ec0f04fdad20a22d5824c97
Egor Kozlov <egkozlov@nes.ru>
2020-01-31 11:33:30 -0600

    Update to intratemporal

--- intratemporal.py
+++ intratemporal.py
@@ -19,42 +19,64 @@ def int_with_x(m_in,newton=True,step=1e-6,nint=2000,*,A,alp,sig,xi,lam,kap,lbr,q
     
     rf_res = lambda x : foc_expression(x) - m_in[0]
     
-    x0 = 0
+    x0 = 1e-6
     
-    for _ in range(5):
+    factor = 1.0
+    
+    for ib in range(15):
         try:
             x0 = brentq(rf_res,step,m_in[0]-step)
             break
         except:
+            if ib == 14: print('brent failed...')
             step = step/10
+            factor = 0.2 # expect slow convergence
             pass
         
-        
     
-    xgrid = np.linspace(x0,m_in.max(),nint)
+    
+    xgrid = np.linspace(0.5*x0,m_in.max(),nint)
     
     
     m_implied = foc_expression(xgrid)
           
-    x_interpolated = np.minimum(np.interp(m_in,m_implied,xgrid),x0)
+    x_interpolated = np.maximum(np.interp(m_in,m_implied,xgrid),x0)
     
-    if not newton:
-        xout = x_interpolated
-    else:
-        # perform 1 iteration of Newton method
+    xout = x_interpolated
+    if newton: 
+        # perform  iteration of Newton method
         def foc_deriv(x):
             logder = ((lam+xi-1)/(sig)) * (x**(lam-1))/(x**lam + kap*(1-lbr)**lam) + \
                 ((1-lam)/sig)/x
             return 1 + (foc_expression(x) - x)*logder
+        
+        
+        for i in range(10):
+            x_start = xout
+            f_res_start = foc_expression(x_start) - m_in
+            f_der = foc_deriv(x_start)
     
-        f_res = foc_expression(x_interpolated) - m_in
-        f_der = foc_deriv(x_interpolated)
+            xout = np.maximum( x_start - factor*f_res_start/f_der, x0 )
     
-        x_improved = x_interpolated - f_res/f_der
-    
-        x_interpolated = x_improved
+            f_res = foc_expression(xout) - m_in
             
-        xout = x_interpolated
+            assert not np.any(np.isnan(f_res))
+            
+            #print('At i = {} max residuals are {}'.format(i,np.abs(f_res).max()))
+            
+            if np.abs(f_res).max() < 1e-4:
+                break
+            if np.abs(f_res).max() > np.abs(f_res_start).max() and i > 2:
+                print('Intratemporal: backing up...')
+                imax = np.abs(f_res).argmax()
+                print('Max residual is {} at x = {}'.
+                      format(f_res[imax],x_start[imax]))                
+                i_worse = (np.abs(f_res) > np.abs(f_res_start))
+                
+                xout[i_worse] = x_start[i_worse]
+                break
+        
+        
     
     
     
