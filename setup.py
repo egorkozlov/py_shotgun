078dc68d80bf3fed9f2e2fd07a35e8ddffc24eb5
Egor Kozlov <egkozlov@nes.ru>
2020-02-06 16:23:07 -0600

    Introducing qbar

--- setup.py
+++ setup.py
@@ -162,7 +162,8 @@ class ModelSetup(object):
         nl = 4
         
         ls = np.linspace(lmin,lmax,nl,dtype=self.dtype)
-        ps = p['pls']*(1-np.linspace(0.0,1.0,nl,dtype=self.dtype))
+        ps = p['pls']*(1-np.linspace(0.0,2.0,nl,dtype=self.dtype))
+        ps[-2:] = 0.0 # !!! think about this carefully
         
         self.ls_levels_nk = np.array([1.0],dtype=self.dtype)
         self.ls_levels_k = ls
