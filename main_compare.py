f6cfc3f04c0c03d8e742539b66091370c376e58f
Egor Kozlov <egkozlov@nes.ru>
2020-03-05 08:13:14 -0600

    Comparison exercise is set

--- main_compare.py
+++ main_compare.py
@@ -26,40 +26,50 @@ os.environ['MKL_CBWR']='AUTO'
  
 if __name__ == '__main__':
      
-    x_base = {'u_shift_mar': 0.725863356303563,
-         'sigma_psi': 0.13579410386649068,
-         'sigma_psi_mult': 4.270601078298901,
-         'pmeet': 0.30889908284645357,
-         'pmeet_t': -0.0038829067012848967,
-         'util_alp': 0.6534190912803465,
-         'util_kap': 1.9136130954048896,
-         'preg_20': 0.03915073600027051,
-         'preg_30': 0.10173830697979773,
-         'high education': True}
+    x_base = {'u_shift_mar': 1.4622468191770523,
+             'sigma_psi': 0.13341343588771484,
+             'sigma_psi_mult': 2.7114359061341653,
+             'pmeet': 0.16330434124234866,
+             'pmeet_t': 0.0,
+             'util_alp': 1.5035902442038718,
+             'util_kap': 7.862043282658021,
+             'preg_20': 0.10034665087728878,
+             'preg_30': 0.01,
+             'util_qbar': 1.7158729365532353,
+             'disutil_marry_sm_mal_coef': 8.047422930867116,
+             'high education': True}
     
     
     
-    x_comp = {'u_shift_mar': 0.78012885,
-         'sigma_psi': 0.22466639,
-         'sigma_psi_mult': 1.34982032,
-         'pmeet': 0.17557838,
-         'pmeet_t': -0.00900235,
-         'util_alp': 0.47896648,
-         'util_kap': 1.72740958,
-         'preg_20': 0.2184985,
-         'preg_30': 0.33067173,
-         'high education': False}
+    x_comp = {'u_shift_mar': 1.4622468191770523,
+             'sigma_psi': 1.97568527e-01,
+             'sigma_psi_mult': 2.60091410,
+             'pmeet': 3.33322285e-01,
+             'pmeet_t': -3.73953602e-04,
+             'util_alp': 1.5035902442038718,
+             'util_kap': 7.862043282658021,
+             'preg_20': 3.07441811e-01,
+             'preg_30': 1.07009581e-01,
+             'util_qbar': 3.44970307e+00,
+             'disutil_marry_sm_mal_coef': 9.89430509e+00,
+             'high education': False}
     
     # form xlist
     xlist = [x_base]
+    
     he = 'high education'
+    
     replace_fields = [[he],
                       ['pmeet','pmeet_t'],
-                      ['pmeet','pmeet_t','preg_20','preg_30'],
+                      ['preg_20','preg_30'],                      
                       ['sigma_psi','sigma_psi_mult'],
-                      ['u_shift_mar','util_kap','util_alp'],
-                      ['sigma_psi','sigma_psi_mult','u_shift_mar','util_kap','util_alp']
+                      ['util_qbar'],
+                      ['disutil_marry_sm_mal_coef'],
+                      ['pmeet','pmeet_t','preg_20','preg_30'],
+                      ['util_qbar','disutil_marry_sm_mal_coef'],
+                      ['util_qbar',he]
                      ]
+    
     for rf in replace_fields:
         xnew = x_base.copy()
         for f in rf:
--- main_onerun.py
+++ main_onerun.py
@@ -29,8 +29,8 @@ os.environ['MKL_CBWR']='AUTO'
 
  
 if __name__ == '__main__':
-     
-    x = {'u_shift_mar': 1.4622468191770523,
+    ''' 
+    x0 = {'u_shift_mar': 1.4622468191770523,
          'sigma_psi': 0.13341343588771484,
          'sigma_psi_mult': 2.7114359061341653,
          'pmeet': 0.16330434124234866,
@@ -41,9 +41,22 @@ if __name__ == '__main__':
          'preg_30': 0.01,
          'util_qbar': 1.7158729365532353,
          'disutil_marry_sm_mal_coef': 8.047422930867116}
+    '''
+    
+    x = {'u_shift_mar': 1.4622468191770523,
+         'sigma_psi': 1.97568527e-01,
+         'sigma_psi_mult': 2.60091410,
+         'pmeet': 3.33322285e-01,
+         'pmeet_t': -3.73953602e-04,
+         'util_alp': 1.5035902442038718,
+         'util_kap': 7.862043282658021,
+         'preg_20': 3.07441811e-01,
+         'preg_30': 1.07009581e-01,
+         'util_qbar': 3.44970307e+00,
+         'disutil_marry_sm_mal_coef': 9.89430509e+00}
 
     
-    tar = target_values('high education')
+    tar = target_values('low education')
     
     out, mdl, agents, res = mdl_resid(x=x,targets=tar,
                                       return_format=['distance','models','agents','scaled residuals'],
