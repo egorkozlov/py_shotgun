cc912e89ed6efdd2882912199389dfa75a053200
Egor Kozlov <egkozlov@nes.ru>
2020-04-22 22:48:29 +0300

    Good, few more tries

--- calibration_params.py
+++ calibration_params.py
@@ -18,19 +18,19 @@ def calibration_params(xin=None,xfix=None):
     # just in case...
     params = OrderedDict(
               sigma_psi=(0.0,0.2,0.16),
-              sigma_psi_mult=(1.0,8.0,5.5),
-              pmeet_0=(0.0,1.0,0.41),
-              pmeet_t=(-0.5,0.5,0.0),
-              pmeet_t2=(-0.1,0.0,0.0),
-              preg_a0=(0.005,0.3,0.03),
-              preg_at=(-0.2,0.2,0.012),
+              sigma_psi_mult=(1.0,8.0,5.4),
+              pmeet_0=(0.0,1.0,0.38),
+              pmeet_t=(-0.5,0.5,0.0059),
+              pmeet_t2=(-0.1,0.0,-0.0005),
+              preg_a0=(0.005,0.3,0.044),
+              preg_at=(-0.2,0.2,0.005),
               preg_at2=(-0.03,0.00,0.00),
-              u_shift_mar=(0.0,2.0,1.75),
-              util_alp=(0.1,4.0,0.70),
-              util_kap=(0.2,5.0,0.71),
-              util_qbar=(0.0,6.0,0.1),
-              disutil_marry_sm_mal_coef = (0.0,10.0,5.5),
-              disutil_shotgun_coef=(0.0,4.0,1.58)
+              u_shift_mar=(0.0,2.0,1.67),
+              util_alp=(0.1,4.0,0.645),
+              util_kap=(0.2,5.0,0.799),
+              util_qbar=(0.0,6.0,0.053),
+              disutil_marry_sm_mal_coef = (0.0,10.0,5.03),
+              disutil_shotgun_coef=(0.0,4.0,1.65)
                         )
              
     
