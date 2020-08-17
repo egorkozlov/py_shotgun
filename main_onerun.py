55e92551312287345773079160e824e9c15228f1
Egor Kozlov <egkozlov@nes.ru>
2020-08-17 17:10:40 +0300

    Adding recent changes

--- main_onerun.py
+++ main_onerun.py
@@ -2,28 +2,28 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan 27
- 
+
 @author: Egor Kozlov
 """
 
 
 #import warnings
 #warnings.filterwarnings("error")
- 
+
 
 from platform import system
-     
-if system() != 'Darwin' and system() != 'Windows':  
+
+if system() != 'Darwin' and system() != 'Windows':
     import os
     os.environ['QT_QPA_PLATFORM']='offscreen'
-    
- 
- 
+
+
+
 from residuals import mdl_resid
 from targets import target_values
 
 print('Hi!')
- 
+
 import os
 os.environ['MKL_CBWR']='AUTO'
 from estimates import get_point
@@ -32,65 +32,64 @@ def main():
     
     high_e = True
     x, targ_mode = get_point(high_e)
-    
-    
-    
-    x = {'sigma_psi': 0.23571313928571477,
-         'sigma_psi_init': 2.123636664878597,
-         'pmeet_21': 0.2873647689783723,
-         'pmeet_28': 0.7553774259051128,
-         'pmeet_35': 1.0,
-         'preg_21': 0.017461033039670462,
-         'preg_28': 0.024901095174551184,
-         'preg_35': -0.015475818457370151, 
-         'u_shift_mar': 1.2306521824428305,
-         'util_alp': 0.3854997739795115,
-         'util_kap': 0.8103116344047732,
-         'util_qbar': 1.0505842569863009,
-         'disutil_marry_sm_mal': 60.0,
-         'disutil_shotgun': 5.581480022921534,
-         'abortion_costs': 30.940624456940288,
-         'p_abortion_access': 0.1799031427804904,
-         'u_lost_divorce': 6.673691715831503}
 
 
 
-    
+    x = {'sigma_psi': 0.24000883142940208,
+        'sigma_psi_init': 2.270662128160131,
+        'pmeet_21': 0.282197067097691,
+        'pmeet_28': 0.717301599527013,
+        'pmeet_35': 0.9998565249924385,
+        'preg_21': 0.027242779157600316,
+        'preg_28': 0.034354384984791576,
+        'preg_35': 0.024562299247326433,
+        'u_shift_mar': 0.8600153678932919,
+        'util_alp': 0.1, 'util_kap': 0.9399627350061319,
+        'util_qbar': 1.2755593891876527,
+        'disutil_marry_sm_mal': 69.13458440788543,
+        'disutil_shotgun': 4.870682077608209,
+        'abortion_costs': 30.308034843501463,
+        'p_abortion_access': 0.8956773350793297,
+        'u_lost_divorce': 5.492799438755628}
+
+
+
+
     tar = target_values(targ_mode)
-    
-    
-    this_name = 'out mar child support'
+
+
+    this_name = 'baseline'
     out, mdl, agents, res, mom = mdl_resid(x=x,targets=tar,
                                       return_format=['distance','models','agents','scaled residuals','moments'],
-                                      #load_from='mdl.pkl',
+                                      save_to='mdl.pkl',
                                       verbose=True,draw=True,cs_moments=False,
                                       moments_save_name = this_name,
                                       moments_repeat=2)
-    
-    mdl[0].time_statistics()
-                         
-    
-    
 
-    
+    mdl[0].time_statistics()
+
+
+
+
+
     print('Done. Residual in point x0 is {}'.format(out))
-    
+
     from targets import all_targets
     tar_fk = all_targets('low education')
     mom_fk = {key: tar_fk[key][0] for key in tar_fk}
-    
-    
+
+
     #from simulations import Agents
     #moments_aux = Agents( mdl, N=10000, T=18, female=False, verbose=False).aux_moments()
-    '''
+
     from fit_plot import FitPlots
     fp = FitPlots(targ_mode=targ_mode,
-                   compare='baseline.pkl',
-                   base=this_name+'.pkl',
-                   compare_name='baseline',
-                   base_name=this_name,
+                   compare='baseline.pkl', #None,
+                   base='no divorce costs.pkl',#this_name+'.pkl',
+                   compare_name='baseline', #'Data',
+                   base_name='no divorce costs',
                    moments_aux=None) #,moments_aux=moments_aux)
-    '''
+
     '''
     from fit_plot import FitPlots
     fp = FitPlots(targ_mode=targ_mode,
@@ -100,9 +99,11 @@ def main():
                    compare_name='sigma zero',
                    #graphs_title_add="Experiment: Removing Subsistence Constraint",
                    moments_aux=None)#,moments_aux=moments_aux)
-    
+
     mdl[0].mar_graphs()
     '''
-    
+    return locals()
+
 if __name__ == '__main__':
-    main()
+    allthings = main()
+    globals().update(**allthings)
