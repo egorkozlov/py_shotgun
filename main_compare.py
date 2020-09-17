33f8d75123e5afa2f7a31c99e9e0fee23553711f
Egor Kozlov <egkozlov@nes.ru>
2020-09-17 19:28:44 +0300

    New version of the education comparison

--- main_compare.py
+++ main_compare.py
@@ -9,6 +9,8 @@ Created on Mon Feb 10 21:54:21 2020
 import dill
 
 from platform import system
+from targets import target_values
+
      
 if system() != 'Darwin' and system() != 'Windows':  
     import os
@@ -23,46 +25,43 @@ print('Hi!')
 import os
 os.environ['MKL_CBWR']='AUTO'
 
+from estimates import get_point
+
  
 if __name__ == '__main__':
      
-    x_base = {'u_shift_mar': 1.4622468191770523,
-             'sigma_psi': 0.13341343588771484,
-             'sigma_psi_mult': 2.7114359061341653,
-             'pmeet_0': 0.16330434124234866,
-             'pmeet_t': 0.0,
-             'pmeet_t2':0.0,
-             'util_alp': 1.5035902442038718,
-             'util_kap': 7.862043282658021,
-             'preg_0': 0.10034665087728878,
-             'preg_t': 0.01,
-             'util_qbar': 1.7158729365532353,
-             'disutil_marry_sm_mal_coef': 8.047422930867116}
     
     
+    x_base, tm = get_point(True,read_wisdom=False) # high education
     
-    x_comp = {'u_shift_mar': 0.78012885,
-         'sigma_psi': 0.22466639,
-         'sigma_psi_mult': 1.34982032,
-         'pmeet': 0.17557838,
-         'pmeet_t': -0.00900235,
-         'util_alp': 0.47896648,
-         'util_kap': 1.72740958,
-         'preg_20': 0.2184985,
-         'preg_30': 0.33067173,
-         'high education': False}
+    tar = target_values(tm)
     
+    x_comp, _ = get_point(False,read_wisdom=False) # low education
+    
+    
+    pmeets = ['pmeet_21','pmeet_30','pmeet_40']
+    ppregs = ['preg_21','preg_28','preg_35']
+    abortions = ['abortion_costs','p_abortion_access']
+    prefs = ['disutil_marry_sm_mal','disutil_shotgun','u_lost_divorce','util_qbar']
     # form xlist
     xlist = [x_base]
-    he = 'high education'
-    replace_fields = [[he],
-                      ['pmeet','pmeet_t'],
-                      ['pmeet','pmeet_t','preg_20','preg_30'],
-                      ['sigma_psi','sigma_psi_mult'],
-                      ['u_shift_mar','util_kap','util_alp'],
-                      ['sigma_psi','sigma_psi_mult','u_shift_mar','util_kap','util_alp']
+    he = ['high education'],
+    replace_fields = [('trend only',he),
+                      ('pmeets',pmeets),
+                      ('ppregs',ppregs),
+                      ('remar',['disutil_marry_sm_mal']),
+                      ('stigma',['disutil_shotgun']),            
+                      ('div costs',['u_lost_divorce']),
+                      ('qbar',['util_qbar']),
+                      ('abortions',abortions),
+                      ('all probs',pmeets+ppregs),
+                      ('all prefs',prefs),
+                      ('trend and all prefs',he+prefs),
+                      ('trend and all probs',he+pmeets+ppregs),   
+                      ('trend and abortions',he+abortions),
+                      ('all but trend',prefs+pmeets+ppregs+abortions)
                      ]
-    for rf in replace_fields:
+    for name, rf in replace_fields:
         xnew = x_base.copy()
         for f in rf:
             xnew[f] = x_comp[f]
@@ -72,16 +71,22 @@ if __name__ == '__main__':
     replace_fields = ['base'] + replace_fields + ['compare']
     xlist.append(x_comp)
     
-    assert False
+    
+    names = ['college'] + [f[0] for f in replace_fields] + ['high school']
+    
+    names = ['educ comparison: '+ n for n in names]
+    
     agents_list = list()
-    for x, rf in zip(xlist,replace_fields):        
-        print(rf)
+    for x, name in zip(xlist,names):        
+        print(name)
         print(x)
-        out, agents = mdl_resid(x=x,return_format=['distance','agents'],
-                                verbose=False,draw=False)
-        agents_list.append(agents)
+        out,  mom = mdl_resid(x=x,targets=tar,
+                                          return_format=['distance','moments'],
+                                          verbose=False,draw=False,cs_moments=False,
+                                          save_to ='mdl for {}'.format(name),
+                                          moments_save_name = name,
+                                          moments_repeat=5)
                          
         print('Done. Residual in point x0 is {}'.format(out))
         
-    dill.dump((agents_list,replace_fields,xlist),open('comparison.pkl','wb+'))            
         
