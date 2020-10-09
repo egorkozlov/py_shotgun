aa118e30201a6037f84f6c92965b136233f2e7da
Egor Kozlov <egkozlov@nes.ru>
2020-10-09 13:24:36 +0300

    Relative to targets

--- targets.py
+++ targets.py
@@ -35,6 +35,7 @@ def target_values(mode='high education'):
             ('ever kids by years after marriage, ',(4,8),1),
             ('k then m in population at ',(23,35),1),
             ('m then k in population at ',(23,35),1),
+            ('k then m in sample at ',(23,35),1),
             ('divorced by years after marriage if kids first, ',(1,10),1/4),
             ('divorced by years after marriage if marriage first, ',(1,10),1/4),
             ('mean x share',),
